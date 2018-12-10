6821ac9686e007d880ff9b94e1f3d81892b3465f
lemonpaul <leamonpaul@yandex.ru>
2018-12-11 00:15:02 +0300

    Add bootstrap styles for authorization form

--- views/login.php
+++ views/login.php
@@ -3,24 +3,21 @@
 	<head>
 		<meta charset="utf8">
 		<title>Заявки на ремонт</title>
+		<link rel="stylesheet" href="../css/bootstrap.css">
+		<link rel="stylesheet" href="../css/signin.css">
+		<script src="../js/bootstrap.js"></script>
 	</head>
-	<body>
-		<div>
-			<h1>Авторизация</h1>
+	<body class="text-center">
 <?php if (isset($_SESSION['WRONG_USER'])): ?>
-			<p><font color="red">Неправильное имя пользователя или пароль.</font></p>
+			<label class="text-danger">Неправильное имя пользователя или пароль.</label>>
 <?php endif ?>
-			<div>
-				<form method="post" action="index.php?action=login">
-					<p>Имя пользователя:</p>
-					<input type="text" name="user" autofocus required>
-					
-					<p>Пароль:</p>
-					<input type="password" name="password" required>
-
-					<br><br><input type="submit" value="Войти">
+				<form class="form-signin" method="post" action="index.php?action=login">
+					<h1 class="h3 mb-3 font-weight-normal">Авторизация</h1>
+					<label for="inputLogin" class="sr-only">Имя пользователя:</label>
+					<input id="inputLogin" class="form-control" placeholder="Имя пользователя" type="text" name="user" autofocus required>
+					<label for="inputPassword:" class="sr-only">Пароль:</label>
+					<input id="inputPassword" class="form-control" placeholder="Пароль" type="password" name="password" required>
+					<button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
 				</form>
-			</div>
-		</div>
 	</body>
 </html>
