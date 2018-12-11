83f0ecd783229f9f3f8740a336c63811910675b0
lemonpaul <leamonpaul@yandex.ru>
2018-12-11 08:58:38 +0300

    Fix login form

--- views/login.php
+++ views/login.php
@@ -8,11 +8,11 @@
 		<script src="../js/bootstrap.js"></script>
 	</head>
 	<body class="text-center">
-<?php if (isset($_SESSION['WRONG_USER'])): ?>
-			<label class="text-danger">Неправильное имя пользователя или пароль.</label>>
-<?php endif ?>
 				<form class="form-signin" method="post" action="index.php?action=login">
 					<h1 class="h3 mb-3 font-weight-normal">Авторизация</h1>
+<?php if (isset($_SESSION['WRONG_USER'])): ?>
+					<label class="text-danger">Неправильное имя пользователя или пароль.</label>
+<?php endif ?>
 					<label for="inputLogin" class="sr-only">Имя пользователя:</label>
 					<input id="inputLogin" class="form-control" placeholder="Имя пользователя" type="text" name="user" autofocus required>
 					<label for="inputPassword:" class="sr-only">Пароль:</label>
